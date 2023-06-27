df65cda12eda3c0249ece670880eb71b2847abc6
Woody Umstead <76594506+woodumstead@users.noreply.github.com>
2023-06-26 21:59:40 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+if (year % 4 === 0) { 
+  if (year % 100 == 0) {
+    if (year % 400 === 0 {
+        console.log(`$(year) is NOT a leap year`);
+    } else { 
+        console.log(`$(year) is NOT a leap year`);
+    }
+  } else { 
+      console.log(`$(year) is a leap year!`);
+  }
+} else { 
+  console.log(`$(year) is NOT a leap year`);
+}
